0721e2d18635cbf954dcb0beed3cf1f71e26cbf9
JerryHuwae <114461659+JerryHuwae@users.noreply.github.com>
2023-06-21 01:51:40 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Resep from './pages/Resep';
+import Upload from './pages/Upload';
+import About from './pages/About';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+
+      {
+        path: '/upload',
+        element: <Upload />,
+      },
+      {
+        path: '/resep',
+        element: <Resep />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
